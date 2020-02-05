d49d99c5784c420052715687f903426d0a9fd777
Roy van de Water <roy.vwater@gmail.com>
2020-02-04 17:01:48 -0700

    Add some boardgame.io stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Client } from "boardgame.io/react";
+import Board from "./Board.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const Trafique = {
+  setup: ctx => ({
+    road: [
+      [1, 0],
+      [0, 0],
+      [0, 0]
+    ],
+    players: {
+      1: {
+        x: 0,
+        y: 0
+      }
+    }
+  }),
+  moves: {
+    keepGoing: (G, ctx) => {players: {"1": {x: 0, y: 2}}},
+    switchLanes: (G, ctx) => null
+  }
+};
+
+const App = Client({ game: Trafique, board: Board, numPlayers: 1 });
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { Trafique } from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Trafique", () => {
+  describe("moves", () => {
+    describe("keepGoing", () => {
+      describe("when the road is clear", () => {
+        let state;
+
+        beforeEach(() => {
+          state = Trafique.moves.keepGoing({
+            road: [
+              [1, 0],
+              [0, 0],
+              [0, 0]
+            ],
+            players: { 1: { x: 0, y: 0 } }
+          });
+        });
+
+        it("should let them go forward", () => {
+          expect(state.players[1]).toMatchObject({
+            x: 0,
+            y: 2
+          });
+        });
+      });
+    });
+  });
 });
