576598ae1b9b321edaacf78dc510520589fa1b46
Roy van de Water <roy.vwater@gmail.com>
2020-02-05 17:41:43 -0700

    Add fancy graphix

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,14 +1,26 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 
-const Board = ({ G }) => (
+import './Board.css'
+
+const isWinner = ctx => ctx.gameover && ctx.gameover.winner
+
+const cellContents = value => {
+  if (value === 'first') return '🚘'
+  if (value === 1) return '🌲'
+  return null
+}
+
+
+const Board = ({ ctx, G }) => (
   <main>
+    <h1>&nbsp;{isWinner(ctx) && 'You Win!'}</h1>
     <table>
       <tbody>
         {G.road.map((row, i) => (
           <tr key={i}>
             {row.map((cell, j) => (
-              <td key={j}>{cell}</td>
+              <td key={j}>{cellContents(cell)}</td>
             ))}
           </tr>
         ))}
@@ -22,6 +34,11 @@ Board.propTypes = {
     road: PropTypes.arrayOf(PropTypes.arrayOf(
       PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
     )),
+  }),
+  ctx: PropTypes.shape({
+    gameover: PropTypes.shape({
+      winner: PropTypes.bool,
+    }),
   }),
 }
 
