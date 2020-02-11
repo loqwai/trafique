1084bb67831809421d1853d3d196a20e82ae3005
royvandewater <royvandewater@users.noreply.github.com>
2020-02-11 18:18:20 +0000

    deploy: f676b867e9e880de3e862676345f3a48236310da

---
+++ precache-manifest.1c14287e41ce5196a48e156389bbe3b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af1e1158465d6ca7ed92da13f6b047f2",
+    "url": "/trafique/index.html"
+  },
+  {
+    "revision": "5c74afc937b82fcf8b7f",
+    "url": "/trafique/static/css/main.a301ceaf.chunk.css"
+  },
+  {
+    "revision": "5f334cd1b8c9bc5c6a9d",
+    "url": "/trafique/static/js/2.c665ec86.chunk.js"
+  },
+  {
+    "revision": "41a40462cc6b0ccf2219289c9a69694f",
+    "url": "/trafique/static/js/2.c665ec86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c74afc937b82fcf8b7f",
+    "url": "/trafique/static/js/main.164509dd.chunk.js"
+  },
+  {
+    "revision": "28e32830dd5f0fc2aa28",
+    "url": "/trafique/static/js/runtime-main.9fd44cc5.js"
+  }
+]);
