5f5825c405a9f90b2fd3e395722134f7433b2ab9
Roy van de Water <roy@circleci.com>
2022-03-08 11:45:35 -0700

    Fix indentation, not sure why it cares

--- src/ecs/Game.js
+++ src/ecs/Game.js
@@ -24,18 +24,16 @@ export class Game {
   /**
    * Private function that runs on each animation frame. If you're trying to
    * run the game, use start() instead
-   */
-   _run = () => {
-     // Compute delta and elapsed time
-     const time = performance.now()
-     const delta = min(time - this.#lastTime, 50)
-     this.#numMsElapsed += delta
-     this.#lastTime = time
+   **/
+  _run = () => {
+    // Compute delta and elapsed time
+    const time = performance.now()
+    const delta = min(time - this.#lastTime, 50)
+    this.#numMsElapsed += delta
+    this.#lastTime = time
 
-     // Run all the systems
-     this.#world.execute(delta, this.#numMsElapsed)
-     this.#animationFrameRequest = requestAnimationFrame(this._run)
-   }
-
-
+    // Run all the systems
+    this.#world.execute(delta, this.#numMsElapsed)
+    this.#animationFrameRequest = requestAnimationFrame(this._run)
+  }
 }
